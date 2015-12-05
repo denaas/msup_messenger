a359f356ff5eb5c7a0729a0230d25aaeaed3b873
helen <helen1995pavlova@gmail.com>
2015-12-05 22:21:32 +0300

    functions for using by server table of (username,hash)

--- database.py
+++ database.py
@@ -14,11 +14,34 @@ db.drop_collection('users')
 
 # Добавление документов в колекцию 'users'
 def insert_user_data(login, hash_sz):
-	db.users.save( { 'login':login, 'hash':hash_sz } )
+	db.users.save( { 'username':login, 'hash':hash_sz } )
 
-insert_user_data(1, 12345)
+def delete_user_data(login):
+	db.users.remove( {'username':login} )
 
+def update_username(login, new_login):
+	db.users.update( {'username':login}, {"$set" : {'username':new_login} } )
+	
+def update_hash(login, new_hash):
+	db.users.update( {'username':login}, {"$set" : {'hash':new_hash} } )
+	
 
 # Получить все документы
-for user in db.users.find():
-    print (user)
+def print_list(db):
+	for user in db.users.find():
+		print (user)
+
+
+#examples	
+insert_user_data(1, 12345)
+insert_user_data(2, 12345)
+insert_user_data(3, 12345)
+
+print_list(db)
+
+delete_user_data(1)
+print_list(db)
+
+update_username(2, 5)
+update_hash(3, 11111)
+print_list(db)
