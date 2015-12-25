c1f93ebe4782d257e895418c61bfc4aae63ffaf6
helen <helen1995pavlova@gmail.com>
2015-12-25 23:44:04 +0300

    sever which work with client(with Windows Forms)

--- server.py
+++ server.py
@@ -2,87 +2,123 @@ from twisted.internet import reactor
 from twisted.internet.protocol import ServerFactory 
 from twisted.protocols.basic import LineOnlyReceiver 
 
-users = {}
+from itertools import compress
+
+#import database
+
+users = { "test": "1", "protest": "1"}
+port = 8001
 
 class ChatProtocol(LineOnlyReceiver): 
 
-    name = "" 
+	name = "" 
+	state = 0 # 1 - auth
+	receiver = None
 
-    def getName(self): 
-        if self.name!="": 
-            return self.name 
-        return self.transport.getPeer().host 
+	def getName(self): 
+		if self.name != "" : 
+			return self.name 
+		return self.transport.getPeer().host 
 
-    def connectionMade(self): 
-        print "New connection from "+self.getName() 
-        self.sendLine("Welcome to my my chat server.") 
-        self.sendLine("Send '/NAME [new name]' to change your name.") 
-        self.sendLine("Send '/EXIT' to quit.") 
-        self.factory.sendMessageToAllClients(self.getName()+" has joined the party.") 
-        self.factory.clientProtocols.append(self)
+	def connectionMade(self): 
+		print ("New connection from " + self.getName()) 
+		#self.factory.sendMessageToAllClients("/SERTIFICATE")
+		self.factory.sendMessageToAllClients(self.getName()+" has joined the party.") 
+		self.factory.clientProtocols.append(self)
 
-    def connectionLost(self, reason): 
-        print "Lost connection from "+self.getName() 
-        self.factory.clientProtocols.remove(self) 
-        self.factory.sendMessageToAllClients(self.getName()+" has disconnected.") 
+	def connectionLost(self, reason): 
+		print ("Lost connection from "+self.getName()) 
+		self.factory.clientProtocols.remove(self) 
+		self.factory.sendMessageToAllClients(self.getName()+" has disconnected.") 
 
-    def register(self, name, password):
-        if not users.get(name) is None:
-            return False
-        users[name] = password
-        return True
+	def register(self, name, password):
+		if not users.get(name) is None:
+			return False
+		self.name = name
+		users[name] = password
+		return True
 
-    def auth(self, name, password):
-        if users.get(name) == password:
-            return name
-        return False
+	def auth(self, name, password):
+		if users.get(name) == password:
+			self.name = name
+			self.state = 1
+			return name
+		self.state = 0
+		return False
 
-    def lineReceived(self, line): 
-        print self.getName()+" said "+line 
-        if line[:5]=="/NAME": 
-            oldName = self.getName() 
-            self.name = line[5:].strip() 
-            self.factory.sendMessageToAllClients(oldName+" changed name to "+self.getName()) 
-        elif line=="/EXIT": 
-            self.transport.loseConnection() 
-        elif line[:len("/REGISTER")]=="/REGISTER":
-            args = line.split(' ')[1:]
-            if len(args) < 2:
-                self.sendLine("/REGISTER FAILURE ARGS")
-            elif self.register(args[0], args[1]):
-                self.sendLine("/REGISTER OK")
-            else:
-                self.sendLine("/REGISTER FAILURE EXISTS")
-        elif line[:len("/AUTH")]=="/AUTH":
-            args = line.split(' ')[1:] + ['', '']
-            auth = self.auth(args[0], args[1])
-            if auth is False:
-                self.sendLine("/AUTH FAILURE")
-            else:
-                self.sendLine("/AUTH OK")
-                self.name = auth
-                self.factory.sendMessageToAllClients("Say hello to " + self.name)
-        elif self.name != "": 
-            self.factory.sendMessageToAllClients(self.getName()+" says: "+line) 
-        else:
-            self.sendLine("Unknown command")
-            
+	def dataReceived(self, line): 
+		print ("<" + self.getName() + "> " + line)
+		args =  line.split(' ')
+		print(len(line))
+		command = args[0]
+		if command=="/EXIT": 
+			self.transport.loseConnection() 
+		elif command=="/REGISTER":
+			if len(args) != 3:
+				self.sendLine("/REGISTER FAILURE ARGS")
+			elif self.register(args[1], args[2]):
+				self.sendLine("/REGISTER OK")
+			else:
+				self.sendLine("/REGISTER FAILURE EXISTS")
+		elif command=="/AUTH":
+			if len(args) != 3 or self.auth(args[1], args[2]) is False:
+				if len(args) == 3:
+					print("not found")
+				self.sendLine("/AUTH FAILURE")
+			else:
+				self.sendLine("/AUTH OK")
+				#self.factory.sendMessageToAuthClients(args[1], "Say hello to " + self.name)
+		elif command=="/MESSAGE":
+			if len(args) >= 3 and self.name != "" and self.state != 0 and args[1] != None:
+				self.factory.sendMessageToAuthClients(args[1], "<" + self.getName() + "> : " + " ".join(args[2:])) 
+			else :
+				print("ERROR")
+				self.sendLine("/ERROR")
+		elif command=="/GROUP_CHAT":
+			pass
+		else:
+			self.sendLine("Unknown command")
+			
 
-    def sendLine(self, line): 
-        self.transport.write(line+"\r\n") 
+	def sendLine(self, line): 
+		self.transport.write(line + "\r\n") 
 
 class ChatProtocolFactory(ServerFactory): 
 
-    protocol = ChatProtocol 
+	protocol = ChatProtocol 
 
-    def __init__(self): 
-        self.clientProtocols = [] 
+	def __init__(self): 
+		self.clientProtocols = [] 
 
-    def sendMessageToAllClients(self, mesg): 
-        for client in self.clientProtocols:
-            client.sendLine(mesg) 
+	def sendMessageToAllClients(self, mesg): 
+		for client in self.clientProtocols:
+			client.sendLine(mesg) 
+	
+	def findUser(self, name):
+		for client in self.clientProtocols:
+			if client.getName() == name:
+				return client
+		return None
+	
+	def sendMessageToAuthClients(self, name, mesg) :  
+		user = self.findUser(name)
+		if user != None and user.state != 0:
+			user.sendLine(mesg)
+		#elif user == None or (user != None and user.state == 0):
+			#self.sendLine("user is not online or doesn't exist")
+		
+			
+	def sendToYou(self, us, message):
+		user = self.findUser(us)
+		if user != None : 
+			user.sendLine(message)
 
-print "Starting Server"
-factory = ChatProtocolFactory()
-reactor.listenTCP(12345, factory)
-reactor.run()
+def main():
+	print ("Starting Server")
+	factory = ChatProtocolFactory()
+	reactor.listenTCP(port, factory)
+	reactor.run()
+
+
+if __name__ == '__main__':
+	main()
