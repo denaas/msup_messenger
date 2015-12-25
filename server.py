b35ba14468a6b398b2e21390006e5f97decdf35f
helen <helen1995pavlova@gmail.com>
2015-12-25 19:56:57 +0300

    server work with client.cpp

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from itertools import compress
 #import database
 
 users = { 'test': '1', 'protest': '1'}
-port = 8000
+port = 8001
 
 class ChatProtocol(LineOnlyReceiver): 
 
@@ -22,13 +22,13 @@ class ChatProtocol(LineOnlyReceiver):
 
 	def connectionMade(self): 
 		print ("New connection from " + self.getName()) 
+		#self.factory.sendMessageToAllClients("/SERTIFICATE")
 		self.factory.sendMessageToAllClients(self.getName()+" has joined the party.") 
 		self.factory.clientProtocols.append(self)
 
 	def connectionLost(self, reason): 
 		print ("Lost connection from "+self.getName()) 
-		#self.factory.clientProtocols.remove(self) 
-		#self.factory.clientAuthProt.remove(self) 
+		self.factory.clientProtocols.remove(self) 
 		self.factory.sendMessageToAllClients(self.getName()+" has disconnected.") 
 
 	def register(self, name, password):
@@ -46,9 +46,10 @@ class ChatProtocol(LineOnlyReceiver):
 		self.state = 0
 		return False
 
-	def lineReceived(self, line): 
-		print (self.getName()+" said " + line)
+	def dataReceived(self, line): 
+		print ("<" + self.getName() + "> " + line)
 		args =  line.split(' ')
+		print(len(line))
 		command = args[0]
 		if command=="/EXIT": 
 			self.transport.loseConnection() 
@@ -60,7 +61,11 @@ class ChatProtocol(LineOnlyReceiver):
 			else:
 				self.sendLine("/REGISTER FAILURE EXISTS")
 		elif command=="/AUTH":
-			if len(args) < 3 or self.auth(args[1], args[2]) is False:
+			if len(args) != 3 or self.auth(args[1], args[2]) is False:
+				if len(args)!= 3:
+					print("!=3")
+				else :
+					print("not found")
 				self.sendLine("/AUTH FAILURE")
 			else:
 				self.sendLine("/AUTH OK")
@@ -100,6 +105,14 @@ class ChatProtocolFactory(ServerFactory):
 		user = self.findUser(name)
 		if user != None and user.state != 0:
 			user.sendLine(mesg)
+		#elif user == None or (user != None and user.state == 0):
+			#self.sendLine("user is not online or doesn't exist")
+		
+			
+	def sendToYou(self, us, message):
+		user = self.findUser(us)
+		if user != None : 
+			user.sendLine(message)
 
 def main():
 	print ("Starting Server")
